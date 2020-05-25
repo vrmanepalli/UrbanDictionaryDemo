2aabff173be506d4f8da27eec9e4c739aaaa8150
Vasudeva Manepalli <vmanepalli@cvent.com>
2020-05-25 10:41:10 -0700

    Added packages and moved file

--- app/src/main/java/com/vmanepalli/urbandictionary/urbandictionarydemo/activities/MainActivity.kt
+++ app/src/main/java/com/vmanepalli/urbandictionary/urbandictionarydemo/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.vmanepalli.urbandictionary.urbandictionarydemo
+package com.vmanepalli.urbandictionary.urbandictionarydemo.activities
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.vmanepalli.urbandictionary.urbandictionarydemo.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
