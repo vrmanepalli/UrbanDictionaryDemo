f76fd2519ace3c282eba7d293b93f784abbe9136
Vasudeva Manepalli <vmanepalli@cvent.com>
2020-05-25 14:29:11 -0700

    Added UI elements and functions to refresh and sort list but only toasts when clicked

--- app/src/main/java/com/vmanepalli/urbandictionary/urbandictionarydemo/activities/MainActivity.kt
+++ app/src/main/java/com/vmanepalli/urbandictionary/urbandictionarydemo/activities/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.vmanepalli.urbandictionary.urbandictionarydemo.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.KeyEvent
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.vmanepalli.urbandictionary.urbandictionarydemo.R
-
+import com.vmanepalli.urbandictionary.urbandictionarydemo.hideKeyboard
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,26 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setSearchListener()
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun setSearchListener() {
+        search.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                search.hideKeyboard()
+                showUnderDevToast()
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    fun sort(view: View?) {
+        showUnderDevToast()
     }
+
+    fun refresh(view: View?) {
+        showUnderDevToast()
+    }
+
+    private fun showUnderDevToast() {
+        Toast.makeText(
+            applicationContext,
+            "Not available! Development in progress..!",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
 }
